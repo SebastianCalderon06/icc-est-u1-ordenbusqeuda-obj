65783aec437b787a9d81d5faa8b75494989c085b
SebastianCalderon06 <sebcalderonm@gmail.com>
2025-04-24 08:54:28 -0500

    Trabajo en Clase

---
+++ src/controllers/PersonaController.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import models.Persona;
+
+public class PersonaController{
+
+
+    public void sortBySeleccionDesbyDireccionCodigo(Persona[] personas){
+        for (int i = 0; i<personas.length; i++){
+            int maxIndex = i;
+            for (int j = i+1; j<personas.length;j++){
+                //if (personas[j].getCodigoDireccion()>personas[maxIndex].getCodigoDireccion()){
+                if(personas[j].equalsByCodigoDireccion(personas[maxIndex])){
+                    maxIndex = j;
+
+                }
+                
+
+            }
+            if (maxIndex != i){
+                Persona temp = personas[maxIndex];
+                personas[maxIndex] = personas[i];
+                personas[i] = temp;
+            }
+        }
+    }
+    public Persona findPersonaByCodigoDireccion(Persona[] personas, int codigo){
+
+        int bajo = 0;
+        int alto = personas.length-1;
+
+        while(bajo<=alto){
+            int central = bajo + (alto-bajo)/2;
+            int valorCentral = personas[central].getCodigoDireccion();
+
+            if(valorCentral == codigo){
+                return personas[central];
+            }
+            if (valorCentral>codigo){
+            bajo = central +1;
+        }else {alto = central - 1;}
+        }
+        return null;
+    }
+
+
+}
+    
+
+
+
+    
+
+
---
+++ src/views/ViewConsole.java
@@ -0,0 +1,11 @@
+package views;
+import models.Persona;
+
+public class ViewConsole {
+
+    public void printPersonas(Persona[] personas){
+
+
+    }
+    
+}
